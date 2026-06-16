fb948c4d68ad8f058e42fbc04229cfdef56b6d5b
nemoraeng-bit <nemoraeng@gmail.com>
2026-06-16 21:37:54 +0300

    إضافة ملف بيانات الحروف الهولندية

---
+++ alphabet.js
@@ -0,0 +1,190 @@
+// بيانات الحروف الهولندية مع الأمثلة والنطق
+const dutchAlphabet = [
+    {
+        letter: 'A',
+        name: 'ايه',
+        example: 'Apple',
+        exampleArabic: 'تفاحة',
+        sound: 'ɑ'
+    },
+    {
+        letter: 'B',
+        name: 'بيه',
+        example: 'Bos',
+        exampleArabic: 'غابة',
+        sound: 'b'
+    },
+    {
+        letter: 'C',
+        name: 'سيه',
+        example: 'Kat',
+        exampleArabic: 'قطة',
+        sound: 'k'
+    },
+    {
+        letter: 'D',
+        name: 'ديه',
+        example: 'Dag',
+        exampleArabic: 'يوم',
+        sound: 'd'
+    },
+    {
+        letter: 'E',
+        name: 'ايه',
+        example: 'Eend',
+        exampleArabic: 'بطة',
+        sound: 'ɛ'
+    },
+    {
+        letter: 'F',
+        name: 'إف',
+        example: 'Fles',
+        exampleArabic: 'زجاجة',
+        sound: 'f'
+    },
+    {
+        letter: 'G',
+        name: 'خيه',
+        example: 'Groep',
+        exampleArabic: 'مجموعة',
+        sound: 'x'
+    },
+    {
+        letter: 'H',
+        name: 'هيه',
+        example: 'Huis',
+        exampleArabic: 'منزل',
+        sound: 'h'
+    },
+    {
+        letter: 'I',
+        name: 'آي',
+        example: 'Ijs',
+        exampleArabic: 'آيس كريم',
+        sound: 'ɪ'
+    },
+    {
+        letter: 'J',
+        name: 'جيه',
+        example: 'Jas',
+        exampleArabic: 'معطف',
+        sound: 'j'
+    },
+    {
+        letter: 'K',
+        name: 'كيه',
+        example: 'Kaas',
+        exampleArabic: 'جبن',
+        sound: 'k'
+    },
+    {
+        letter: 'L',
+        name: 'إل',
+        example: 'Lamp',
+        exampleArabic: 'مصباح',
+        sound: 'l'
+    },
+    {
+        letter: 'M',
+        name: 'إم',
+        example: 'Maan',
+        exampleArabic: 'قمر',
+        sound: 'm'
+    },
+    {
+        letter: 'N',
+        name: 'إن',
+        example: 'Neus',
+        exampleArabic: 'أنف',
+        sound: 'n'
+    },
+    {
+        letter: 'O',
+        name: 'أو',
+        example: 'Oog',
+        exampleArabic: 'عين',
+        sound: 'ɔ'
+    },
+    {
+        letter: 'P',
+        name: 'بيه',
+        example: 'Pen',
+        exampleArabic: 'قلم',
+        sound: 'p'
+    },
+    {
+        letter: 'Q',
+        name: 'كيو',
+        example: 'Qua',
+        exampleArabic: 'كوا',
+        sound: 'kw'
+    },
+    {
+        letter: 'R',
+        name: 'آر',
+        example: 'Rood',
+        exampleArabic: 'أحمر',
+        sound: 'r'
+    },
+    {
+        letter: 'S',
+        name: 'إس',
+        example: 'Ster',
+        exampleArabic: 'نجمة',
+        sound: 's'
+    },
+    {
+        letter: 'T',
+        name: 'تيه',
+        example: 'Tafel',
+        exampleArabic: 'طاولة',
+        sound: 't'
+    },
+    {
+        letter: 'U',
+        name: 'يو',
+        example: 'Uit',
+        exampleArabic: 'خارج',
+        sound: 'ʏ'
+    },
+    {
+        letter: 'V',
+        name: 'فيه',
+        example: 'Vogel',
+        exampleArabic: 'طائر',
+        sound: 'v'
+    },
+    {
+        letter: 'W',
+        name: 'دبليو',
+        example: 'Water',
+        exampleArabic: 'ماء',
+        sound: 'w'
+    },
+    {
+        letter: 'X',
+        name: 'إكس',
+        example: 'Xylofoon',
+        exampleArabic: 'إكسيليفون',
+        sound: 'ks'
+    },
+    {
+        letter: 'Y',
+        name: 'واي',
+        example: 'Yogurt',
+        exampleArabic: 'يوغورت',
+        sound: 'j'
+    },
+    {
+        letter: 'Z',
+        name: 'زيد',
+        example: 'Zebra',
+        exampleArabic: 'حمار وحشي',
+        sound: 'z'
+    }
+];
+
+// تصدير البيانات
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = dutchAlphabet;
+}
